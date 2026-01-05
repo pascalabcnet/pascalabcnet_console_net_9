4c45abd68890ec4a5bb30ddb10a62883d2f9a273
Ivan Bondarev <ibond84@googlemail.com>
2026-01-05 12:17:45 +0100

    fixed Localization.csproj fixed LanguageResMaker

---
+++ Utils/DefaultLanguageResMaker/LanguageResMaker.pas
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Ivan Bondarev, Stanislav Mikhalkovich (for details please see \doc\copyright.txt)
+// This code is distributed under the GNU LGPL (for details please see \doc\license.txt)
+uses 
+  FilesOperations,
+  System.IO,
+  System.Resources;
+
+begin
+  var Files := DirectoryInfo.Create(Path.Combine('..', '..', 'bin', 'shared', 'Lng', 'Rus')).GetFiles('*.*');
+  Writeln('Найдено ' + Files.Length.tostring + ' файлов, обработка...');
+  var res := '';
+  for var i := 0 to Files.Length - 1 do
+    res := res + #13#10#13#10'//' + Files[i].Name + #13#10'%PREFIX%='#13#10 +
+    	ReadFileToEnd(Files[i].FullName);
+  Writeln('Сохранение собраного файла в ресурс...');
+  var ResWriter := ResourceWriter.Create(Path.Combine('..', '..', 'src', 'Localization', 'DefaultLang.resources'));
+  ResWriter.AddResource('DefaultLanguage', System.Text.Encoding.GetEncoding(1251).GetBytes(res));
+  ResWriter.Generate;
+  ResWriter.Close;
+  Writeln('OK');
+end.
